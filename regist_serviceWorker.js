f2e79a2df285f9a5bd10085af4d2f4ad249bba82
REDUX Redux <moiredux@gmail.com>
2022-04-11 13:37:37 -0500

    third commit

--- regist_serviceWorker.js
+++ regist_serviceWorker.js
@@ -4,12 +4,3 @@ if ('serviceWorker' in navigator) {
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
 
-/* tambien se puede registrar el servi WORKER de esta manera
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("./Service_Worker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-} */
